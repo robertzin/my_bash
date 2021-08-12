125f79146840e46c7a0d3e9e1abb0da12dd5d4ef
Yetta Jama <yjama@ev-s1.kzn.21-school.ru>
2021-08-12 15:55:27 +0300

    push

---
+++ minishell.h
@@ -0,0 +1,16 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h> 
+# include <dirent.h>
+# include <string.h>
+# include <time.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <term.h>
+
+#endif
