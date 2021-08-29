342f4f66ff758081d5b8e29664de3fa0c97ed7db
Robert Zinyatullin <robert_zin@MacBook-Pro-Robert.local>
2021-08-29 18:02:50 +0300

    2

--- ft_exec_echo.c
+++ ft_exec_echo.c
@@ -9,20 +9,17 @@ void	ft_exec_echo(t_cmd *cmd)
 	flag = 0;
 	if (cmd->cmd && cmd->cmd[i])
 	{
+		if (ft_strcmp(cmd->cmd[i], "-n") == 0)
+		{
+			flag = 1;
+			if (cmd->cmd[++i] == NULL)
+				ft_putstr_fd("", 1);
+		}
 		while (cmd->cmd[i] != NULL)
 		{
-			if (ft_strcmp(cmd->cmd[i], "-n") == 0)
-			{
-				flag = 1;
-				if (cmd->cmd[++i] == NULL)
-					ft_putstr_fd("", 1);
-			}
-			else
-			{
-				ft_putstr_fd(cmd->cmd[i], 1);
-				if (cmd->cmd[++i] != NULL)
-					ft_putchar_fd(' ', 1);
-			}
+			ft_putstr_fd(cmd->cmd[i], 1);
+			if (cmd->cmd[++i] != NULL)
+				ft_putchar_fd(' ', 1);
 		}
 	}
 	if (flag == 0)
